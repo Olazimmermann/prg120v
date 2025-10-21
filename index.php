d26f05b85d0f66b42f64c1c752caa17d4c6671ea
Olazimmermann <olazb@sandefjordbredband.net>
2025-10-21 13:13:41 +0200

    Add SQL table creation for klassekode

--- index.php
+++ index.php
@@ -1,1 +1,7 @@
-<?php /*Klasse*/>
+<?php /*Klasse*/
+CREATE TABLE klassekode (
+klassekode CHAR(5) NOT NULL,
+klassenavn VARCHAR(50) NOT NULL,
+stadiumkode VARCHAR(50) NOT NULL,
+PRIMARY KEY(klassekode));
+?>
