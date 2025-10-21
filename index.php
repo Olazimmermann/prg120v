6d9b760995cb7f19e12c5d0b69ca7a00bbed999d
Olazimmermann <olazb@sandefjordbredband.net>
2025-10-21 13:26:14 +0200

    Update SQL table creation syntax for klasse

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
-<?php /*Klasse*/
-CREATE klassekode (
-klassekode CHAR(5) NOT NULL,
-klassenavn VARCHAR(50) NOT NULL,
-stadiumkode VARCHAR(50) NOT NULL,
-PRIMARY KEY(klassekode));
+<?php 
+create table if not exists klasse (
+  klassekode CHAR(5) NOT NULL,
+  klassenavn VARCHAR(50) NOT NULL,
+  stadiumkode VARCHAR(50) NOT NULL,
+  PRIMARY KEY (klassekode)
+  ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
 ?>
