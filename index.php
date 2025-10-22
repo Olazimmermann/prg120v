77b372c1187cf06f63a103eda3e688091a3881ec
Olazimmermann <olazb@sandefjordbredband.net>
2025-10-22 15:53:43 +0200

    Update index.php

--- index.php
+++ index.php
@@ -14,3 +14,31 @@
 
   <button type="submit" name="lagre_klasse">Lagre</button>
 </form>
+
+
+/* Registrer student*/
+<h2>Registrer student</h2>
+<form method="post" action="index.php">
+  <label>Brukernavn (max 7):<br>
+    <input type="text" name="brukernavn" maxlength="7" required>
+  </label><br><br>
+
+  <label>Fornavn:<br>
+    <input type="text" name="fornavn" maxlength="50" required>
+  </label><br><br>
+
+  <label>Etternavn:<br>
+    <input type="text" name="etternavn" maxlength="50" required>
+  </label><br><br>
+
+  <label>Klassekode:<br>
+    <select name="klassekode" required>
+      <option value="">-- velg --</option>
+      <option value="IT1">IT1</option>
+      <option value="IT2">IT2</option>
+      <option value="IT3">IT3</option>
+    </select>
+  </label><br><br>
+
+  <button type="submit" name="lagre_student">Lagre</button>
+</form>
