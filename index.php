586810e6f696eafab3676027123dcb6135ed78b4
Olazimmermann <olazb@sandefjordbredband.net>
2025-10-23 14:35:49 +0200

    ny

--- index.php
+++ index.php
@@ -14,14 +14,8 @@ $database = getenv('DB_DATABASE');
 
     $sqlResultat=mysqli_query($db,$sqlSetning) or die ("ikke mulig &aring; hente data fra databasen");
     /* SQL-setning sendt til database-serveren */
+  
+    $antallRader=mysqli_num_rows($sqlResultat);  /* antall rader i resultatet beregnet */ 
 
-    if (mysqli_num_rows($sqlResultat) > 0) {
-        while ($rad = mysqli_fetch_assoc($sqlResultat)) {
-            echo "ID: " . $rad["id"] . " - Navn: " . $rad["navn"] . "<br>";
-        }
-    } else {
-        echo "Ingen resultater funnet.";
-    }
-
-    mysqli_close($db);
+    
 ?>
