3ee36d1b47862f00a315d0b6d545034cb0fb1e22
Olazimmermann <olazb@sandefjordbredband.net>
2025-10-23 13:36:03 +0200

    k

--- index.php
+++ index.php
@@ -10,3 +10,7 @@ $database = getenv('DB_DATABASE');
  $db=mysqli_connect($host,$username,$password,$database) or die ("ikke kontakt med database-server");
     /* tilkobling til database-serveren utført */
  ?> 
+
+<?php
+$sql = "SELECT * FROM `klasse`;";
+?>
